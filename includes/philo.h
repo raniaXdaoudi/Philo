92faa9b8da12a32f9b140c2882fbe1b6609d84d1
Rania Daoudi Idrissi <rania@MacBook-Pro-de-Rania.local>
2023-01-16 21:00:47 +0100

    gerer time sleep

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: rania <rania@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 17:40:14 by rania             #+#    #+#             */
-/*   Updated: 2023/01/12 17:52:47 by rania            ###   ########.fr       */
+/*   Updated: 2023/01/16 20:47:09 by rania            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 # define RED "\033[0;31m"
 # define WHITE "\033[0;37m"
@@ -29,6 +30,13 @@ typedef struct s_stack
 	int				time_sleep;
 	int				nb_meal;
 }	t_stack;
+
+typedef struct s_philo
+{
+	int			id;
+	pthread_t	thread;
+	t_stack		*stack;
+}	t_philo;
 
 int		ft_check_arg(char **av);
 int		ft_isdigit(int c);
--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,19 @@
 /*   By: rania <rania@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 17:43:31 by rania             #+#    #+#             */
-/*   Updated: 2023/01/12 18:52:01 by rania            ###   ########.fr       */
+/*   Updated: 2023/01/16 20:59:59 by rania            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+time_t	get_current_time(void)
+{
+	struct timeval	tval;
+
+	gettimeofday(&tval, NULL);
+	return ((tval.tv_sec * 1000) + (tval.tv_usec / 1000));
+}
 
 t_stack	*ft_fill_table(char **av, int ac)
 {
@@ -26,43 +34,73 @@ t_stack	*ft_fill_table(char **av, int ac)
 		table->nb_meal = ft_atoi(av[5]);
 	return (table);
 }
-void *thread_1(pthread_mutex_t forks[2])
+void *routine(void *data)
 {
-	pthread_mutex_lock(&forks[0]);
-	pthread_mutex_lock(&forks[1]);
-	printf("Je suis un philo qui graille.\n");
-	pthread_mutex_unlock(&forks[0]);
-	pthread_mutex_unlock(&forks[1]);
+	t_philo	*philo;
+
+	philo = (t_philo *)data;
+	// pthread_mutex_lock(&forks[0]);
+	// pthread_mutex_lock(&forks[1]);
+	printf("%lu %i is thinking.\n", get_current_time(), philo->id);
+	usleep(philo->stack->time_sleep);
+	printf("%lu %i is sleeping.\n", get_current_time(), philo->id);
+	// pthread_mutex_unlock(&forks[0]);
+	// pthread_mutex_unlock(&forks[1]);
 	return NULL;
 }
-int main(void)
-{
-	// t_stack	*table;
 
-	// if (ac != 5 && ac != 6)
-	// {
-	// 	printf(RED"Bad number of args.\n"WHITE);
-	// 	return (0);
-	// }
-	// if (!ft_check_arg(av))
-	// 	return (0);
-	// table = ft_fill_table(av, ac);
-	pthread_t	philo[2];
-	pthread_mutex_t    forks[2];
+t_philo	**ft_init_philo(t_stack *table)
+{
+	t_philo		**philo;
 	int			i;
 
 	i = -1;
-	pthread_mutex_init(&forks[0], NULL);
-	pthread_mutex_init(&forks[1], NULL);
-	while(++i < 2)
+	philo = malloc(sizeof(t_philo *) * table->nb_philo);
+	if (!philo)
+		return (0);
+	while (++i < table->nb_philo)
 	{
-		pthread_create(&philo[i], NULL, thread_1, forks);
+		philo[i] = malloc(sizeof(t_philo));
+		if (!philo[i])
+			return (0);
+		philo[i]->id = i + 1;
+		philo[i]->stack = table;
 	}
+	return (philo);
+}
+
+void	ft_open_thread(t_philo **philo, t_stack *table)
+{
+	int	i;
+
 	i = -1;
-	while(++i < 2)
+	while(++i < table->nb_philo)
+		pthread_create(&philo[i]->thread, NULL, routine, philo[i]);
+}
+int main(int ac, char **av)
+{
+	t_stack	*table;
+	t_philo	**philo;
+	int		i;
+
+	if (ac != 5 && ac != 6)
 	{
-		pthread_join(philo[i], NULL);
-		pthread_mutex_destroy(&forks[i]);
+		printf(RED"Bad number of args.\n"WHITE);
+		return (0);
+	}
+	if (!ft_check_arg(av))
+		return (0);
+	table = ft_fill_table(av, ac);
+	philo = ft_init_philo(table);
+	ft_open_thread(philo, table);
+	i = -1;
+	// pthread_mutex_t    forks[2];
+	// pthread_mutex_init(&forks[0], NULL);
+	// pthread_mutex_init(&forks[1], NULL);
+	while(++i < table->nb_philo)
+	{
+		pthread_join(philo[i]->thread, NULL);
+		// pthread_mutex_destroy(&forks[i]);
 	}
 	return (0);
 }
